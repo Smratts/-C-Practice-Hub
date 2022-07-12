ecfec19fe298dc2b3efce4051315d30f7ee04e28
Mahender <codingmahender@gmail.com>
2022-07-12 13:06:32 +0530

    sum c prog

---
+++ data.c
@@ -0,0 +1,14 @@
+// find datatype size
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+    int a;
+    float b;
+    int c;
+    clrscr();
+    printf(" %d \n", sizeof(a));
+    printf("%f\n", sizeof(b));
+    printf(" %i\n", sizeof(a));
+    getch();
+ }
---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+    int a,b,c ;
+    printf("enter two number:");
+    scanf("%i %i", &a,&b);
+    c=a+b;
+    printf("sum of two numbers given:%i",c);
+    return 0;
+ }

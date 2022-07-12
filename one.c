bad4589405f91edbc9e8dc26c205d4cf4c436ca9
Mahender <codingmahender@gmail.com>
2022-07-12 17:51:17 +0530

    c int short commits

---
+++ one.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+ {
+unsigned short x=-32772;
+//clrscr();
+printf("%i \n",x);
+printf("%u \n",x);
+ }
---
+++ sign.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+ {
+short x=32769;
+//clrscr();
+printf("%i \n",x);
+ }

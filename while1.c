f2b4a69b74ac78dc955575d2cbf622468fbe495e
Mahender <codingmahender@gmail.com>
2022-07-14 12:20:24 +0530

    while loop commit

---
+++ while1.c
@@ -0,0 +1,24 @@
+// #include<stdio.h>
+// int main ()
+// {
+//     int b,c;
+//     printf("enter the values for b and c %i %i", &b,&c);
+//     int a=1;
+//     while(a <=b)
+//     {
+//         printf("%i\n",a);
+//         a++;
+        
+//     }
+// }
+#include<stdio.h>
+int main ()
+{
+    int a=1;
+    while(a <=10)
+    {
+        printf("%i\n",a);
+        a++;
+        
+    }
+}
